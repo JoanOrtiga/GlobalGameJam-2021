4108717d93b3fa0c5030204ec56cf091760c217b
Helnyr <Helnyr@users.noreply.github.com>
2021-01-29 16:50:46 +0100

    main menu

---
+++ GlobalGameJam2021/Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+    public GameObject creditsMenu;
+
+    IEnumerator LoadingAsync (int index)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(index);
+
+        while (!asyncLoad.isDone)
+        {
+            Debug.Log(asyncLoad.progress);
+            yield return null;
+        }
+    }
+
+    public void StartGame(int scene)
+    {
+        StartCoroutine(LoadingAsync(scene));
+    }
+
+    public void MenuMain()
+    {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+        creditsMenu.SetActive(false);
+    }
+
+    public void Options()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+        creditsMenu.SetActive(false);
+    }
+
+    public void Credits()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(false);
+        creditsMenu.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
