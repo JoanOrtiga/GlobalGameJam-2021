11b0a4961d99a6a8c345ba560d55e982d8a266f4
JoanStark <47462893+JoanStark@users.noreply.github.com>
2021-01-30 23:34:24 +0100

    Pause | GameManager

--- GlobalGameJam2021/Assets/Scripts/NextScene.cs
+++ GlobalGameJam2021/Assets/Scripts/NextScene.cs
@@ -9,6 +9,6 @@ public class NextScene : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        SceneManager.LoadScene(sceneToLoad);      
+        GameManager.instance.NextScene(sceneToLoad);
     }
 }
